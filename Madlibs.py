f9f2fa312198a409b001ec8f08af1f7886779909
Vitor Odorico <100388613+VitorOdorico@users.noreply.github.com>
2022-05-21 03:00:02 -0300

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,10 @@
+# Madlibs
+adj = input("Adijetivo: ")
+verb1 = input("Verbo: ")
+verb2 = input("Verbo: ")
+famous_person = input("Personagem famoso: ")
+
+madlib = f"Programação de computador é tão  {adj}!Isso me deixa tão animado o tempo todo porque \ eu amo {verb1}. Mantenha-se hidratado e {verb2} como se fosse  {famous_person}!"
+
+print(madlib)
+
